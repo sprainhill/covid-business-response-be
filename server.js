d69dfc7c75aac070fed326a2f3694d72f584403d
Rory Murray <rory@tacen.app>
2020-03-09 20:19:56 -0400

    Install dependencies, setup server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const server = express();
+
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({server: "running"});
+});
