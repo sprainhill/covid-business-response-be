9f3e91f501d4e962f51193e91756c4a45144038e
Rory Murray <rory@tacen.app>
2020-03-10 23:03:18 -0400

    Add more CRUD functionality

--- models/responseModel.js
+++ models/responseModel.js
@@ -4,10 +4,38 @@ module.exports = {
     getResponses
 }
 
+// get all responses
 function getResponses() {
     return db('responses');
 }
 
+// get response by id
 function getResponseById() {
     return db('responses').where({ id })
+}
+
+// post a new response
+
+function postResponse(response) {
+    return db('responses')
+    .insert(response, 'id')
+    .then(ids => {
+        const [id] = ids;
+        return getResponseById(id);
+    })
+}
+
+// edit a response by id
+function editResponse(id, response) {
+    return db('responses')
+    .where({ id })
+    .update(response)
+    .then(updateId => getResponseById(updateId));
+}
+
+// delete a response by id
+function deleteResponse(id) {
+    return db('responses')
+    .where('id', id)
+    .del();
 }
